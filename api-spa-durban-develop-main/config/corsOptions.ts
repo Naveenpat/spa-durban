3cb5f2212d91fe52d2e437bd2b7916488e589e8c
Naveen Patel <naveenpat261@gmail.com>
2025-06-06 14:01:28 +0530

    issue fix cors and env

--- api-spa-durban-develop-main/config/corsOptions.ts
+++ api-spa-durban-develop-main/config/corsOptions.ts
@@ -2,7 +2,7 @@ import config from "./config";
 
 // ./config/corsOptions.ts
 const corsOptions = {
-  origin:"*",
+ origin: `${config.allowed_origins}`,
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   allowedHeaders: ["Content-Type", "Authorization", "Device-id"],
 };
